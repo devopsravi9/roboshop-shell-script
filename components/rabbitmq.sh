a31c3ca53d480f6c4005e49d2a94ffbb33226a5c
Ravi <ravi@gmail.com>
2022-06-16 12:42:10 +0530

    roboshop project setup

---
+++ components/rabbitmq.sh
@@ -0,0 +1,20 @@
+frontend:
+  bash components/frontend.sh
+mongodb:
+  bash components/mongodb
+catalogue:
+  bash components/catalogue
+redis:
+  bash components/redis
+user:
+  bash components/user
+cart:
+  bash components/cart
+mysql:
+  bash components/mysql
+shipping:
+  bash components/shipping
+rabbitmq:
+  bash components/rabbitmq
+payment:
+  bash components/payment
