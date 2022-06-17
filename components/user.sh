f2e410626548b9884cbb2d0c5fcc1cecb9f68dfc
Ravi <ravi@gmail.com>
2022-06-17 16:50:37 +0530

    user code

--- components/user.sh
+++ components/user.sh
@@ -8,7 +8,7 @@ useradd roboshop
 curl -s -L -o /tmp/user.zip "https://github.com/roboshop-devops-project/user/archive/main.zip"
 cd /home/roboshop
 rm -rf user
-unzip /tmp/user.zip
+unzip -o /tmp/user.zip
 mv user-main user
 cd /home/roboshop/user
 npm install
