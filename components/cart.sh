10ee185a665258832a4b70202a25781be7a2d514
Ravi <ravi@gmail.com>
2022-06-17 19:32:31 +0530

    cart code

---
+++ components/cart.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+
+
+
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+yum install nodejs -y
+useradd roboshop
+curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"
+cd /home/roboshop
+rm -rf cart
+unzip -o /tmp/cart.zip
+mv cart-main cart
+cd cart
+npm install
+
+# Update `REDIS_ENDPOINT` with REDIS server IP Address
+# Update `CATALOGUE_ENDPOINT` with Catalogue server IP address
+
+sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' /home/roboshop/cart/systemd.service
+mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service
+systemctl daemon-reload
+systemctl start cart
+systemctl enable cart
