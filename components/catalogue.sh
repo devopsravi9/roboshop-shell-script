00b92be2046884a5e3d0184e43f499f993555d77
Ravi <ravi@gmail.com>
2022-06-17 15:57:23 +0530

    catalogue code

--- components/catalogue.sh
+++ components/catalogue.sh
@@ -7,6 +7,7 @@ useradd roboshop
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
 cd /home/roboshop
 rm -rf catalogue
+# removing catalogue file for smooth re run code
 unzip -o /tmp/catalogue.zip
 # -o added for overwrite existing during re run of code
 mv catalogue-main catalogue
@@ -15,7 +16,7 @@ npm install
 
 
 #    Update `MONGO_DNSNAME` with MongoDB Server IP
-
+sed -i -e 's/MONGO_DNSNAME/mogodb.roboshop.internal/' /etc/systemd/system/catalogue.service
 mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
 systemctl daemon-reload
 systemctl start catalogue
