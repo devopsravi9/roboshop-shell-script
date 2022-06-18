7abd9af532583e4b97e3cab92f48cc6014346c83
Ravi <ravi@gmail.com>
2022-06-18 11:35:06 +0530

    cart code

--- components/frontend.sh
+++ components/frontend.sh
@@ -16,14 +16,14 @@ rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 systemctl restart nginx
 
-#sed -i -e '/api\/catalogue/ s/localhost/catalogue.roboshop.internal/' /etc/nginx/default.d/roboshop.conf
+sed -i -e '/api\/catalogue/ s/localhost/catalogue.roboshop.internal/' /etc/nginx/default.d/roboshop.conf
 #sed -i -e '/api\/user/ s/localhost/user.roboshop.internal/' /etc/nginx/default.d/roboshop.conf
 #sed -i -e '/api\/cart/ s/localhost/cart.roboshop.internal/' /etc/nginx/default.d/roboshop.conf
 #sed -i -e '/api\/shipping/ s/localhost/shipping.roboshop.internal/' /etc/nginx/default.d/roboshop.conf
 #sed -i -e '/api\/payment/ s/localhost/payment.roboshop.internal/' /etc/nginx/default.d/roboshop.conf
 
 #systemctl daemon-reload
-#systemctl restart nginx
+systemctl restart nginx
 #    location /api/catalogue/ { proxy_pass http://localhost:8080/; }
 
    # location /api/user/ { proxy_pass http://localhost:8080/; }
