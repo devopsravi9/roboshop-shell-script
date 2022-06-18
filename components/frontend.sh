a558f7d797285451823c18c6e87e8feb1893f304
Ravi <ravi@gmail.com>
2022-06-18 11:31:38 +0530

    cart code

--- components/frontend.sh
+++ components/frontend.sh
@@ -16,11 +16,11 @@ rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 systemctl restart nginx
 
-sed -i -e '/api\/catalogue/ s/localhost/catalogue.roboshop.internal/' /etc/nginx/default.d/roboshop.conf
-sed -i -e '/api\/user/ s/localhost/user.roboshop.internal/' /etc/nginx/default.d/roboshop.conf
-sed -i -e '/api\/cart/ s/localhost/cart.roboshop.internal/' /etc/nginx/default.d/roboshop.conf
-sed -i -e '/api\/shipping/ s/localhost/shipping.roboshop.internal/' /etc/nginx/default.d/roboshop.conf
-sed -i -e '/api\/payment/ s/localhost/payment.roboshop.internal/' /etc/nginx/default.d/roboshop.conf
+#sed -i -e '/api\/catalogue/ s/localhost/catalogue.roboshop.internal/' /etc/nginx/default.d/roboshop.conf
+#sed -i -e '/api\/user/ s/localhost/user.roboshop.internal/' /etc/nginx/default.d/roboshop.conf
+#sed -i -e '/api\/cart/ s/localhost/cart.roboshop.internal/' /etc/nginx/default.d/roboshop.conf
+#sed -i -e '/api\/shipping/ s/localhost/shipping.roboshop.internal/' /etc/nginx/default.d/roboshop.conf
+#sed -i -e '/api\/payment/ s/localhost/payment.roboshop.internal/' /etc/nginx/default.d/roboshop.conf
 
 #systemctl daemon-reload
 #systemctl restart nginx
