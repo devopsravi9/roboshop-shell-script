0abcfd0d0b0f1d1845570309f767974d3f7b0d6e
Ravi <ravi@gmail.com>
2022-06-18 11:50:15 +0530

    cart code

--- components/redis.sh
+++ components/redis.sh
@@ -5,6 +5,7 @@ yum install redis-6.2.7 -y
 
 
 sed -i -e 's/127.0.0.1./0.0.0.0./' /etc/redis.conf
-#sed -i -e 's/127.0.0.1./0.0.0.0./' /etc/redis/redis.conf
-#systemctl enable redis
-#systemctl start redis
+sed -i -e 's/127.0.0.1./0.0.0.0./' /etc/redis/redis.conf
+
+systemctl enable redis
+systemctl start redis
