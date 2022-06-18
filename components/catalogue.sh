2d7cf228e0e12ab3131b8a85390e9abab182774b
Ravi <ravi@gmail.com>
2022-06-18 09:44:32 +0530

    cart code

--- components/catalogue.sh
+++ components/catalogue.sh
@@ -16,7 +16,7 @@ npm install
 
 
 #    Update `MONGO_DNSNAME` with MongoDB Server IP
-sed -i -e 's/MONGO_DNSNAME/mogodb.roboshop.internal/' /home/roboshop/catalogue/systemd.service
+sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/catalogue/systemd.service
 mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
 systemctl daemon-reload
 systemctl start catalogue
