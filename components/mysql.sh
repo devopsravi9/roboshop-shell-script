7eccf83ca0d366b5e38769ac874ce0b28e0e387b
Ravi <ravi@gmail.com>
2022-06-18 15:35:42 +0530

    cart code

--- components/mysql.sh
+++ components/mysql.sh
@@ -1,19 +1,18 @@
 
 
+
 curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
 yum install mysql-community-server -y
-
 systemctl enable mysqld
 systemctl start mysqld
 
-#grep temp /var/log/mysqld.log
+MYSQL_DEFAULT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
 
-# mysql_secure_installation
-# mysql -uroot -pRoboShop@1
-# curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';" | mysql --connect-expired-password -uroot -p"${MYSQL_DEFAULT_PASSWORD}"
+echo "uninstall plugin validate_password;" | mysql -uroot -p"${MYSQL_PASSWORD}"
 
-# cd /tmp
-# unzip mysql.zip
-# cd mysql-main
-# mysql -u root -pRoboShop@1 <shipping.sql
-```
+curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
+cd /tmp
+unzip -o mysql.zip
+cd mysql-main
+mysql -u root -p"${MYSQL_PASSWORD}" <shipping.sql
