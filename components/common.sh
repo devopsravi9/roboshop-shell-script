95681fa85b753f807266e57b494b5e25a0c47a27
Ravi <ravi@gmail.com>
2022-06-20 13:53:41 +0530

    ec2 code

--- components/common.sh
+++ components/common.sh
@@ -3,8 +3,7 @@
 CHECK_ROOT () {
   USER_ID=$(id -u)
 if [ $USER_ID -ne 0 ]; then
-    echo you are not a root user
-    echo you can run the script as root user or use sudo command
+    echo -e "\e[31m you should run this script as root user or use sudo in command. \e[0m"
     exit 2
 fi
 }
