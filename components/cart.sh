d1b2ec024794d474e8a91f279b1417c4ce2840ef
Ravi <ravi@gmail.com>
2022-06-20 15:04:38 +0530

    ec2 code

--- components/cart.sh
+++ components/cart.sh
@@ -7,21 +7,46 @@ echo "setting nodejs YUM repo"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>> $LOG
 CHECK_STAT $?
 
-yum install nodejs -y
-useradd roboshop
-curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"
+echo "installing nodejs"
+yum install nodejs -y &>> $LOG
+CHECK_STAT $?
+
+echo "creating roboshop user"
+useradd roboshop &>> $LOG
+CHECK_STAT $?
+
+echo "downloading cart content"
+curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip" &>> $LOG
+CHECK_STAT $?
+
 cd /home/roboshop
 rm -rf cart
-unzip -o /tmp/cart.zip
+
+echo "unziping cart file"
+unzip -o /tmp/cart.zip &>> $LOG
+CHECK_STAT $?
+
 mv cart-main cart
 cd cart
-npm install
+
+echo "installing nodejs depedencies for cart component"
+npm install &>> $LOG
+CHECK_STAT $?
 
 # Update `REDIS_ENDPOINT` with REDIS server IP Address
 # Update `CATALOGUE_ENDPOINT` with Catalogue server IP address
 
-sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' /home/roboshop/cart/systemd.service
+echo "updating systemD configuration"
+sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' /home/roboshop/cart/systemd.service &>> $LOG
+CHECK_STAT $?
+
 mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service
+
 systemctl daemon-reload
 systemctl start cart
-systemctl enable cart
+
+echo "start cart service"
+systemctl enable cart &>> $LOG
+CHECK_STAT $?
+
+
