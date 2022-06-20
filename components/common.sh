3e995b44ce34d7b7b7c010f60d73012715f73cc3
Ravi <ravi@gmail.com>
2022-06-20 14:44:49 +0530

    ec2 code

--- components/common.sh
+++ components/common.sh
@@ -2,16 +2,16 @@
 
 CHECK_ROOT () {
   USER_ID=$(id -u)
-if [ $USER_ID -ne 0 ]; then
+if [ $USER_ID -ne 0 ] ; then
     echo -e "\e[31m you should run this script as root user or use sudo in command. \e[0m"
     exit 2
 fi
 }
 
 CHECK_STAT () {
-  if [$1 -ne 0 ]; then
+  if [$1 -eq 0 ] ; then
     echo -e "\e[31m FAILURE \e[0m"
-    exit 2
+    #exit 2
   else
     echo -e "\e[33m SUCCESS \e[0m"
 fi
