609cd6e83eacead440cfe4bb7da5cdaa53808c91
Ravi <ravi@gmail.com>
2022-06-21 11:54:53 +0530

    ec2 code

--- components/common.sh
+++ components/common.sh
@@ -56,11 +56,11 @@ APP_COMMON_SETUP () {
 
 SYSTEMD () {
     PRINT "updating systemD configuration"
-    sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/cart/systemd.service &>> $LOG
+    sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/${component}/systemd.service &>> $LOG
     CHECK_STAT $?
 
     PRINT "setup systemd configuration"
-    mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service && systemctl daemon-reload &>> $LOG
+    mv /home/roboshop/${component}/systemd.service /etc/systemd/system/${component}.service && systemctl daemon-reload &>> $LOG
     CHECK_STAT $?
 
     PRINT "start ${component} service"
