3d246c9e7af922cd69fafb42890f281348642786
Ravi <ravi@gmail.com>
2022-06-21 14:50:26 +0530

    ec2 code

--- components/user.sh
+++ components/user.sh
@@ -1,22 +1,9 @@
 #!/usr/bin/bash
 
 source components/common.sh
-CHECK_ROOT
 
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
-useradd roboshop
+component=user
+NODE_JS
 
-curl -s -L -o /tmp/user.zip "https://github.com/roboshop-devops-project/user/archive/main.zip"
-cd /home/roboshop
-rm -rf user
-unzip -o /tmp/user.zip
-mv user-main user
-cd /home/roboshop/user
-npm install
 
-sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' /home/roboshop/user/systemd.service
-mv /home/roboshop/user/systemd.service /etc/systemd/system/user.service
-systemctl daemon-reload
-systemctl start user
-systemctl enable user
+#sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' /home/roboshop/user/systemd.service
