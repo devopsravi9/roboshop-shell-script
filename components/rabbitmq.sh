22b9c890fc1367b5487162d731659e0b7aa8f345
Ravi <ravi@gmail.com>
2022-06-28 11:44:50 +0530

    ec2 code

--- components/rabbitmq.sh
+++ components/rabbitmq.sh
@@ -13,13 +13,8 @@ PRINT "Setup Yum Repos"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>>${LOG}
 CHECK_STAT $?
 
-
-PRINT " settingup rabbitmq repo file"
-curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>>$LOG
-CHECK_STAT $?
-
-PRINT "installing rabbitmq"
-yum install rabbitmq-server -y &>>$LOG
+PRINT "Install ErLang & Rabbitmq"
+yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm rabbitmq-server -y &>>${LOG}
 CHECK_STAT $?
 
 PRINT "starting rabbitmq-server"
