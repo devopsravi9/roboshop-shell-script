b65e8071ad32ef3080f517b3ff2b4c7d9dd5066d
Ravi <ravi@gmail.com>
2022-09-08 14:31:59 +0530

    ec2 code

--- component/frontend.sh
+++ component/frontend.sh
@@ -1,42 +1,43 @@
 
 source component/common.sh
 
-yum install nginx -y
+touch /tmp/out
+yum install nginx -y &>> /tmp/out
 x=$?
 CHECKSTAT $x
 
-systemctl enable nginx
+systemctl enable nginx &>> /tmp/out
 x=$?
 CHECKSTAT $x
 
-systemctl start nginx
+systemctl start nginx &>> /tmp/out
 x=$?
 CHECKSTAT $x
 
-curl -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+curl -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>> /tmp/out
 x=$?
 CHECKSTAT $x
 
-cd /usr/share/nginx/html
+cd /usr/share/nginx/html &>> /tmp/out
 x=$?
 CHECKSTAT $x
 
-rm -rf *
+rm -rf * &>> /tmp/out
 x=$?
 CHECKSTAT $x
 
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip &>> /tmp/out
 x=$?
 CHECKSTAT $x
 
-mv frontend-main/static/* .
+mv frontend-main/static/* . &>> /tmp/out
 x=$?
 CHECKSTAT $x
 
-mv frontend-main/localhost.conf /etc/nginx/system.d/roboshop.conf
+mv frontend-main/localhost.conf /etc/nginx/system.d/roboshop.conf &>> /tmp/out
 x=$?
 CHECKSTAT $x
 
-systemctl restart nginx
+systemctl restart nginx &>> /tmp/out
 x=$?
 CHECKSTAT $x
