72e55c10290b05e4fdadcc5def1e9f8f7dfa574a
Ravi <ravi@gmail.com>
2022-09-08 14:29:24 +0530

    ec2 code

---
+++ component/common.sh
@@ -0,0 +1,8 @@
+CHECKSTAT () {
+  if [ $1 -ne 0 ]; then
+    echo -e "\e[31m failure \e[0m"
+    exit 1
+  else
+    echo sucess
+  fi
+}
--- component/frontend.sh
+++ component/frontend.sh
@@ -1,10 +1,42 @@
+
+source component/common.sh
+
 yum install nginx -y
+x=$?
+CHECKSTAT $x
+
 systemctl enable nginx
+x=$?
+CHECKSTAT $x
+
 systemctl start nginx
+x=$?
+CHECKSTAT $x
+
 curl -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+x=$?
+CHECKSTAT $x
+
 cd /usr/share/nginx/html
+x=$?
+CHECKSTAT $x
+
 rm -rf *
+x=$?
+CHECKSTAT $x
+
 unzip /tmp/frontend.zip
+x=$?
+CHECKSTAT $x
+
 mv frontend-main/static/* .
+x=$?
+CHECKSTAT $x
+
 mv frontend-main/localhost.conf /etc/nginx/system.d/roboshop.conf
+x=$?
+CHECKSTAT $x
+
 systemctl restart nginx
+x=$?
+CHECKSTAT $x
