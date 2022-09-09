f8864e3e7b23ff2619e0bc5e414d8f0ebf28e17d
Ravi <ravi@gmail.com>
2022-09-09 11:35:33 +0530

    ec2 code

--- component/mongodb.sh
+++ component/mongodb.sh
@@ -12,9 +12,26 @@ PRINT "installing mongod"
 yum install -y mongodb-org &>> $LOG
 CHECKSTAT $?
 
+PRINT "updating listen ip address"
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>> $LOG
+CHECKSTAT $?
+
 PRINT "enable & start mongod"
 systemctl enable mongod && systemctl start mongod &>> $LOG
 CHECKSTAT $?
+
+PRINT "download mongod schema files"
+curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
+CHECKSTAT $?
+
+PRINT "organizing mongod schema files"
+cd /tmp && unzip mongodb.zip && cd mongodb-main &>> $LOG
+CHECKSTAT $?
+
+PRINT "load catlogue & users schema file to mongod"
+mongo < catalogue.js &&  mongo < users.js
+CHECKSTAT $?
+
 
 
 
