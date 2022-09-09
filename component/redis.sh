d6f9085fb35f10ca01ce9408f399f09a1082a4e9
Ravi <ravi@gmail.com>
2022-09-09 14:10:07 +0530

    ec2 code

--- component/redis.sh
+++ component/redis.sh
@@ -3,9 +3,18 @@ CHECKROOT
 LOG
 
 PRINT "downloading redis repo file"
-curl -L -o /etc/yum.repos.d/redis.repo https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo
+curl -L -o /etc/yum.repos.d/redis.repo https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo &>> $LOG
 CHECKSTAT $?
 
 PRINT "installing redis "
-yum install redis-6.2.7 -y
+yum install redis-6.2.7 -y &>> $LOG
 CHECKSTAT $?
+
+PRINT "update listening ip address URLs"
+sed -i -e '/^bind/s/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf && sed -i -e '/^bind/s/127.0.0.1/0.0.0.0/' /etc/redis.conf &>> $LOG
+CHECKSTAT $?
+
+PRINT " enable, start redis"
+systemctl enable redis && systemctl start redis &>> $LOG
+CHECKSTAT $?
+
