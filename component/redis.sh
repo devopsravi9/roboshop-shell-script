c815c2a374ccbd9e3ac56a88109787c4c6ad6de5
Ravi <ravi@gmail.com>
2022-09-09 12:34:49 +0530

    ec2 code

--- component/redis.sh
+++ component/redis.sh
@@ -1,3 +1,8 @@
+source component/common.sh
+CHECKROOT
+LOG
 
-curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo
+curl -L -o /etc/yum.repos.d/redis.repo https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo
 yum install redis-6.2.7 -y
+systemctl enable redis
+systemctl start redis
