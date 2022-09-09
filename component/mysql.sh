69d3033f8880e1f3bcdcddeaa3ccb4f4b486e76a
Ravi <ravi@gmail.com>
2022-09-09 15:05:16 +0530

    ec2 code

---
+++ component/mysql.sh
@@ -0,0 +1,11 @@
+source component/common.sh
+CHECKROOT
+LOG
+
+
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
+
+yum install mysql-community-server -y
+
+# systemctl enable mysqld
+# systemctl start mysqld
