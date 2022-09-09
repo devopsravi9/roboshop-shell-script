72d1bf21392a77927978b7057561ccb3c48ac34f
Ravi <ravi@gmail.com>
2022-09-09 09:39:02 +0530

    ec2 code

--- component/mongodb.sh
+++ component/mongodb.sh
@@ -3,8 +3,14 @@ LOG
 
 PRINT "setting mongo repo file"
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>> $LOG
-CHeCKSTAT
+CHECKSTAT
 
-# yum install -y mongodb-org
-# systemctl enable mongod
-# systemctl start mongod
+PRINT "installing mongod"
+yum install -y mongodb-org &>> $LOG
+CHECKSTAT
+
+PRINT "enable & start mongod"
+systemctl enable mongod && systemctl start mongod &>> $LOG
+CHECKSTAT
+
+
