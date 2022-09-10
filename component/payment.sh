92cca518377ad29275c568ae268464b58f9d127e
Ravi <ravi@gmail.com>
2022-09-10 12:37:05 +0530

    ec2 code

--- component/payment.sh
+++ component/payment.sh
@@ -4,18 +4,18 @@ LOG
 COMPONENT=payment
 
 PRINT "installing python3"
-yum install python36 gcc python3-devel -y
+yum install python36 gcc python3-devel -y &>> $LOG
 CHECKSTAT $?
 
 APP_COMMON_SETUP
 
 PRINT "installing python dependencies"
-pip3 install -r requirements.txt
+pip3 install -r requirements.txt &>> $LOG
 CHECKSTAT $?
 
 USER=$(id -u roboshop)
 GROUP=$(id -g roboshop)
 
-sed -i -e "/uid/ s/1/${USER}/" -e "/gid/ c gid = ${GROUP}" /${COMPONENT}/payment.ini
+sed -i -e "/uid/ s/1/${USER}/" -e "/gid/ c gid = ${GROUP}" /home/roboshop/${COMPONENT}/payment.ini
 
 SYSTEMD
