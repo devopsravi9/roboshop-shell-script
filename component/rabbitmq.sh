8f014d6d37b89777982ccfd43dde5dd5b158d650
Ravi <ravi@gmail.com>
2022-09-10 16:12:42 +0530

    ec2 code

--- component/rabbitmq.sh
+++ component/rabbitmq.sh
@@ -11,19 +11,15 @@ PRINT "setup rabbitmq repo file"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>> ${LOG}
 CHECKSTAT $?
 
-PRINT "downloding erlang depenencies"
+PRINT "Install ErLang & Rabbitmq"
 yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm  rabbitmq-server -y &>> ${LOG}
 CHECKSTAT $?
-
-#PRINT "installing rabbitmq"
-#yum install rabbitmq-server -y &>> ${LOG}
-#CHECKSTAT $?
 
 PRINT "enable & start rabbitmq"
 systemctl enable rabbitmq-server  &>> ${LOG} && systemctl start rabbitmq-server
 CHECKSTAT $?
 
-sudo rabbitmqctl list_users | grep roboshop
+sudo rabbitmqctl list_users | grep roboshop  &>> ${LOG}          
 if [ $? -ne 0 ]; then
   PRINT "adding roboshop user"
   rabbitmqctl add_user roboshop "${RABBITMQ_PASSWORD}"
