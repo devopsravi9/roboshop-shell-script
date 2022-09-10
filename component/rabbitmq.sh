ace790e9125cd732ed32e792973d8fd3eecaf4f3
Ravi <ravi@gmail.com>
2022-09-10 12:02:34 +0530

    ec2 code

---
+++ component/rabbitmq.sh
@@ -0,0 +1,24 @@
+source component/common.sh
+CHECKROOT
+LOG
+
+PRINT "downloding erlang depenencies"
+yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y &>> ${LOG}
+CHECKSTAT $?
+
+PRINT "setup rabbitmq repo file"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>> ${LOG}
+CHECKSTAT $?
+
+PRINT "installing rabbitmq"
+yum install rabbitmq-server -y &>> ${LOG}
+CHECKSTAT $?
+
+PRINT "enable & start rabbitmq"
+systemctl enable rabbitmq-server  &>> ${LOG} && systemctl start rabbitmq-server
+CHECKSTAT $?
+
+rabbitmqctl add_user roboshop roboshop123
+rabbitmqctl set_user_tags roboshop administrator
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+
