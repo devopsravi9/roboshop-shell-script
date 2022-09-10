60117d9a9a98b57710e8b94abce1ecf000f942b5
Ravi <ravi@gmail.com>
2022-09-10 12:51:11 +0530

    ec2 code

--- component/common.sh
+++ component/common.sh
@@ -95,7 +95,9 @@ PRINT "organizing the content"
 mv ${component}-main/static/* . && mv ${component}-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>> $LOG
 CHECKSTAT $?
 
-sed -i
+PRINT "updating ip adresses"
+sed -i -e '/catalogue/s/localhost/catalogue.roboshop.internal/' -e '/user/s/localhost/user.roboshop.internal/' -e '/cart/s/localhost/cart.roboshop.internal/' -e '/shipping/s/localhost/shipping.roboshop.internal/' -e '/payment/s/localhost/payment.roboshop.internal/' /etc/nginx/default.d/roboshop.conf
+CHECKSTAT $?
 
 PRINT "enable & start service"
 systemctl enable nginx  &>> $LOG && systemctl start nginx &>> $LOG
