2b0df89163fb225fc9e919956cd31888b258b286
Ravi <ravi@gmail.com>
2022-09-10 12:18:27 +0530

    ec2 code

--- component/payment.sh
+++ component/payment.sh
@@ -13,8 +13,8 @@ PRINT "installing python dependencies"
 pip3 install -r requirements.txt
 CHECKSTAT $?
 
-sed -i -e "/uid/ c /${USER}" -e "/gid/ c /${GROUP}" /${COMPONENT}/payment.ini
+#sed -i -e "/uid/ c /${USER}" -e "/gid/ c /${GROUP}" /${COMPONENT}/payment.ini
 
 # Update the roboshop user and group id in payment.ini file.
 
-SYSTEMD
+#SYSTEMD
